307ea3524907867264c9ad862b5060764ad24903
rixcx <adts0415.ovo@gmail.com>
2021-12-19 22:47:42 +0900

    [index] ハンバーガメニューの実装

---
+++ js/toggle.js
@@ -0,0 +1,21 @@
+$('.header__togglebtn').click (function(){
+  $(this).toggleClass('active');
+  $('.nav').toggleClass('open');
+});
+
+$('.nav__list a').click (function(){
+  $('.header__togglebtn').removeClass("active");
+  $('.nav').removeClass('open');
+});
+
+$(function(){
+  $('a[href^="#"]').click (function(){
+    var headerheight= 63;
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top - headerheight;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
